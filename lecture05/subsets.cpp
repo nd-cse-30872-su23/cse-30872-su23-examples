07d67372fcb40700bff62af58fbf116c8eccf08e
Peter Bui <pbui@nd.edu>
2023-06-18 14:19:36 -0400

    Lecture 05: harmonize

--- lecture05/subsets.cpp
+++ lecture05/subsets.cpp
@@ -8,18 +8,18 @@ using namespace std;
 
 // Functions
 
-size_t subsets(vector<int> &s, vector<int> &d, size_t k) {
-    if (k == d.size()) { // Base: have complete subset
+size_t subsets(vector<int> &s, vector<int> &c, size_t k) {
+    if (k == c.size()) { // Base: have complete subset
     	// for (auto e : s) { cout << " " << e; }; cout << endl;
     	return (accumulate(s.begin(), s.end(), 0) % 3 == 0) ? 1 : 0;
     }
 
     // Recurse: skip current
-    size_t count1 = subsets(s, d, k + 1);
+    size_t count1 = subsets(s, c, k + 1);
 
     // Recurse: with current
-    s.push_back(d[k]);
-    size_t count2 = subsets(s, d, k + 1);
+    s.push_back(c[k]);
+    size_t count2 = subsets(s, c, k + 1);
     s.pop_back(); // Reset subset
     
     return count1 + count2;
